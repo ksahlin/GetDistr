6a33f44542a18a4441de3c2db699fb3b445b431c
ksahlin <krsahlin@gmail.com>
2014-12-15 10:51:27 +0000

    fixed display X bug

--- scripts/reapr/plot_fp_tp.py
+++ scripts/reapr/plot_fp_tp.py
@@ -1,6 +1,8 @@
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import argparse
 # import matplotlib.pyplot as plt
 # import numpy as np
