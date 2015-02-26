3cc43c34159e7d0a9ec1abeac6d8c6579454a6bd
ksahlin <krsahlin@gmail.com>
2015-02-26 19:47:55 +0100

    first test scripts for experiments on uppmax

---
+++ scripts/correcting_bias/rhodo_mp_uppmax.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH -A b2013169
+#SBATCH -p core -n 1
+#SBATCH -t 03:00:00
+#SBATCH -J rhodo-MP
+
+
+#####
+#Module dependencies
+#none
+#####
+
+#########
+#TO SPECIFY
+INBASE='/proj/b2013169/private/data/genomes/rhodo/'
+OUTBASE='/home/kris/Work/GetDistr/two_bias/rhodo/'
+
+#######
+/proj/b2013169/private/data/genomes/rhodo
+
+#######
+rm -r "$OUTBASE"
+mkdir "$OUTBASE"
+
+
+/usr/bin/time -v python main.py pipeline \
+       "$INBASE"aligned/ref/gage_mp.bam \
+        "$INBASE"/ref/genome.fasta \
+        "$OUTBASE" \
+        --plots 1> "$OUTBASE"run.stdout 2> "$OUTBASE"run.stdout
+
+
+
+
+
---
+++ scripts/correcting_bias/staph_mp_uppmax.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH -A b2013169
+#SBATCH -p core -n 1
+#SBATCH -t 02:00:00
+#SBATCH -J staph-MP
+
+
+#####
+#Module dependencies
+#none
+#####
+
+#########
+#TO SPECIFY
+INBASE='/proj/b2013169/private/data/genomes/staph/'
+OUTBASE='/home/kris/Work/GetDistr/two_bias/staph/'
+
+#######
+/proj/b2013169/private/data/genomes/staph
+
+#######
+rm -r "$OUTBASE"
+mkdir "$OUTBASE"
+
+
+/usr/bin/time -v python main.py pipeline \
+       "$INBASE"aligned/ref/gage_mp.bam \
+        "$INBASE"/ref/genome.fasta \
+        "$OUTBASE" \
+        --plots 1> "$OUTBASE"run.stdout 2> "$OUTBASE"run.stdout
+
+
+
+
+
