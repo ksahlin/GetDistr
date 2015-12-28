8ce4b48e5aef37a4c0c4dc09c23ed5dbd11801a5
Kristoffer Sahlin <kxs624@psu.edu>
2015-12-28 18:39:24 +0000

    fix

--- getdistr/assemblymodule/filter_bam.py
+++ getdistr/assemblymodule/filter_bam.py
@@ -11,7 +11,7 @@ try:
 	import matplotlib.pyplot as plt
 	import seaborn as sns
 	sns.set_palette("husl", desat=.6)
-	sns.set(font_scale=2)
+	sns.set(font_scale=1.6)
 	plt.rcParams.update({'font.size': 22})
 except ImportError:
 	pass
--- getdistr/assemblymodule/find_normal_parameters.py
+++ getdistr/assemblymodule/find_normal_parameters.py
@@ -12,7 +12,7 @@ try:
 	import matplotlib.mlab as mlab
 	import seaborn as sns
 	sns.set_palette("husl", desat=.6)
-	sns.set(font_scale=2)
+	sns.set(font_scale=1.6)
 	plt.rcParams.update({'font.size': 22})
 except ImportError:
 	pass
